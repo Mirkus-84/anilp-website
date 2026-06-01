b4ccf85be3e347d9ba01155d1caf8151d52357b4
Mirkus-84 <mirko.nanocchio@gmail.com>
2026-06-01 19:31:46 +0200

    Crop hero logo transparent whitespace

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,20 +11,30 @@ export function Logo({ compact = false, prominent = false }: LogoProps) {
     : '/Sfondo%20ANILP%20trasparente.png'
   const alt = compact ? 'Logo ANILP compatto' : 'Logo ANILP'
 
+  if (prominent) {
+    return (
+      <Link to="/" className="inline-flex items-center" aria-label="ANILP home">
+        <span className="block h-[150px] w-[320px] max-w-full overflow-hidden md:h-[320px] md:w-[760px] lg:h-[380px] lg:w-[920px]">
+          <img
+            src={src}
+            width={920}
+            height={613}
+            alt={alt}
+            className="h-auto w-[320px] max-w-none -translate-y-[38px] object-contain md:w-[760px] md:-translate-y-[92px] lg:w-[920px] lg:-translate-y-[112px]"
+          />
+        </span>
+      </Link>
+    )
+  }
+
   return (
     <Link to="/" className="inline-flex items-center" aria-label="ANILP home">
       <img
         src={src}
-        width={prominent ? 340 : 176}
-        height={prominent ? 239 : 124}
+        width={176}
+        height={124}
         alt={alt}
-        className={`${
-          prominent
-            ? 'h-[260px] w-[320px] max-w-full object-contain md:h-[320px] md:w-[760px] lg:h-[360px] lg:w-[920px]'
-            : compact
-              ? 'h-10 w-auto md:h-12'
-              : 'h-14 w-auto md:h-16'
-        }`}
+        className={`${compact ? 'h-10 w-auto md:h-12' : 'h-14 w-auto md:h-16'}`}
       />
     </Link>
   )
